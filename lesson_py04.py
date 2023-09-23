d91b7014936494395aa1de474d5e13163d1f4a70
ndevbogado <ndevbogado@gmail.com>
2023-09-23 19:57:43 -0300

    feat: create python lesson 04 file

---
+++ lesson_py04.py
@@ -0,0 +1,13 @@
+def main():
+    x = int(input("What's x? "))
+    y = int(input("What's y? "))
+
+    if x < y:
+        print("X is less than y")
+    elif x > y:
+        print("x is greater than y")
+    else:
+        print("x is equal to y")
+
+
+main()
