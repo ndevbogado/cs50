68185c90ac0392d98bd6c26860862081cce7f65d
ndevbogado <ndevbogado@gmail.com>
2023-09-23 18:18:51 -0300

    feat: create python-lesson 02 file

---
+++ lesson_py02.py
@@ -0,0 +1,11 @@
+x = int(input("What's x? "))
+
+y = int(input("What's y? "))
+
+z =  x / y
+
+# With this line we can see that python start reading the line, starting from 'X' input
+#z = int(input("What's x? ")) + int(input("What's y? " ))
+
+
+print(z)
