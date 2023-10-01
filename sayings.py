6089d2ee8969a288765876d592b75f485ca07a2c
ndevbogado <ndevbogado@gmail.com>
2023-10-01 17:39:47 -0300

    feat: create python-lesson-13 and example file
    Python lesson 13: import own modules with python

---
+++ sayings.py
@@ -0,0 +1,12 @@
+def main():
+    hello("XD")
+    goodbye("XD")
+
+def hello(name):
+    print(f"Hello There! {name}")
+
+def goodbye(name):
+    print(f"General {name}! You are a bold-one")
+
+if __name__ == "__main__":
+    main()
