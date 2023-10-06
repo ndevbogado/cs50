23098093d601266a09db8f2c20ceb9fb9d6359cd
ndevbogado <ndevbogado@gmail.com>
2023-10-06 00:49:39 -0300

    feat: update python-lesson-16
    Python Lesson 16: Continue with I/o and lambda functions

--- factions.py
+++ factions.py
@@ -1,4 +1,17 @@
+
+heroes = []
+
 with open("factions.csv") as file:
     for line in file:
-        hero,faction = line.rstrip().split(",")
-        print(f"{hero} is a {faction}")     
+        name,faction = line.rstrip().split(",")
+        hero = {"name":name,"faction":faction}
+
+        heroes.append(hero)
+
+"""
+def get_name(hero):
+    return hero['name']
+"""
+
+for hero in sorted(heroes, key=lambda hero: hero["name"]):
+    print(f"{hero['name']} is a {hero['faction']}")
