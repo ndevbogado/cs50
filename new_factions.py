c0d4d1ad0a81484b27d88524ef41e4c67c51eba1
ndevbogado <ndevbogado@gmail.com>
2023-10-08 16:44:33 -0300

    update: modify new_faction.py content
    Python lesson 16: modify new_faction.py code to be more robust at
    order changes

--- new_factions.py
+++ new_factions.py
@@ -3,9 +3,9 @@ import csv
 heroes = []
 
 with open("factions.csv") as file:
-    reader = csv.reader(file)
+    reader = csv.DictReader(file)
 
     for row in reader:
-        heroes.append({"name": row[0], "faction": row[1]})
+        heroes.append({"name": row["hero"], "faction": row["faction"]})
 for hero in sorted(heroes, key=lambda hero: hero["name"]):
     print(f"{hero['name']} is a {hero['faction']}")
